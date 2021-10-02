1f8564d44afb84ec7a1fb1e36e6b7c38ae96f3e7
huydevct <huyvipct1@gmail.com>
2021-10-02 21:46:38 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan("combined"));
+
+//Template engine
+app.engine("hbs", handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log('Path: ', path.join(__dirname, 'resources/views'));
+
+app.get("/", (req, res) => {
+  res.render('home');
+});
+
+
+app.get("/news", (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
