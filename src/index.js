ca9798b395f74de0f680edcbd3d984750d147331
huydevct <huyvipct1@gmail.com>
2021-10-03 18:00:55 +0700

    default form

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 //HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //Template engine
 app.engine("hbs", handlebars({
@@ -24,7 +24,13 @@ app.get("/", (req, res) => {
 
 
 app.get("/news", (req, res) => {
+  console.log(req.query.q)
   res.render('news');
+});
+
+
+app.get("/search", (req, res) => {
+  res.render('search');
 });
 
 app.listen(port, () => {
