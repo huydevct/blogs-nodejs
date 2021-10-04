0125e2d482427f6b7b70755e2108534c7914bc70
huydevct <huyvipct1@gmail.com>
2021-10-04 18:12:25 +0700

    test lint-staged

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-      const route = require('./routes/index');
+const route = require('./routes/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
