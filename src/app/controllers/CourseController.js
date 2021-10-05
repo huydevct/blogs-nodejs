e5997fb34be5098d71bbbbfc7438a636b2ac029f
huydevct <huyvipct1@gmail.com>
2021-10-06 00:35:47 +0700

    delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -45,6 +45,13 @@ class CourseController {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
+
+    //[DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -9,6 +9,7 @@ router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
+router.delete('/:id', courseController.delete);
 router.get('/:slug', courseController.show);
 
 module.exports = router;
