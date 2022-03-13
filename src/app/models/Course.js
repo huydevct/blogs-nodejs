8825967896c7212bcc3b8b3c7ed662f717a06ae8
huydevct <huyctdev@example.com>
2022-03-13 17:56:17 +0700

    update

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator');
-const mongooseDelete = require('mongoose-delete');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
+const mongooseDelete = require("mongoose-delete");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema(
-    {
-        _id: { type: Number },
-        name: { type: String, required: true },
-        description: { type: String },
-        image: { type: String },
-        videoId: { type: String, required: true },
-        slug: { type: String, slug: 'name', unique: true },
-        level: { type: String },
-    },
-    {
-        _id: false,
-        timestamps: true,
-    },
+  {
+    _id: { type: Number },
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true },
+    slug: { type: String, slug: "name", unique: true },
+    level: { type: String },
+  },
+  {
+    _id: false,
+    timestamps: true,
+  }
 );
 
 //Add plugin
@@ -26,8 +26,8 @@ mongoose.plugin(slug);
 
 Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, {
-    deletedAt: true,
-    overrideMethods: 'all',
+  deletedAt: true,
+  overrideMethods: "all",
 });
 
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model("Course", Course);
